428324fa1a825b9545ed317cf7744fd5518aeedd
rodrigocaputo <rodrigokaputo@gmail.com>
2021-05-16 22:56:05 -0300

    Refactoring the functional testing to eliminate redundancy

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,12 @@ class NewVsitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	# Auxiliary method 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith ouviu falar de uma nova aplicação online interessante
 		# para lista de tarefas. Ela decide verificar a homepage
@@ -38,17 +44,13 @@ class NewVsitorTest(unittest.TestCase):
 
 		inputbox.send_keys('Buy peacock featers')
 
-
 		# Quando ela tecla enter, a página é atualizada, e agora
 		# a página lista "1 - Buy peacock feathers" como um item em 
 		# uma lista de tarefas
 
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock featers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock featers')
 
 		# Ainda continua havendo uma caixa de texto convidando-a a 
 		# acrescentar outro item. Ela insere "Use peacock feathers 
@@ -61,11 +63,8 @@ class NewVsitorTest(unittest.TestCase):
 
 		# A página é atualizada novamente e agora mostra os dois
 		# itens em sua lista
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock featers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', 
-			[row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock featers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith se pergunta se o site lembrará de sua lista. Então
 		# ela nota que o site gerou um URL único para ela -- há um 
