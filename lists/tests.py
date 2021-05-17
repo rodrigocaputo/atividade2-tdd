3fee80c7b97633112cb5a6018a24995bd75fe10f
rodrigocaputo <rodrigokaputo@gmail.com>
2021-05-16 23:08:18 -0300

    Correcting view to deal with normal request and request via POST

--- lists/tests.py
+++ lists/tests.py
@@ -12,10 +12,20 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
+	def test_only_saves_items_when_necessary(self):
+		self.client.get('/')
+		self.assertEquals(Item.objects.count(), 0)
+
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
+
+		self.assertEquals(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEquals(new_item.text, 'A new list item')
+
 		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
+
 
 from lists.models import Item
 
@@ -38,3 +48,4 @@ class ItemModelTest(TestCase):
 
 		self.assertEquals(first_saved_item.text, 'The first (ever) list item')
 		self.assertEquals(second_saved_item.text, 'Item the second')
+
